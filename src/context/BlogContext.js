400fc08a1354b2ae15370f94b3074cad7f3baf59
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-08 08:17:05 +0300

    lisätty useStaten avulla blogPosts tila ja setfuntio. Molemmat valutettu alaspäin childreneille.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,20 +1,26 @@
-import React, { Children } from 'react';
+import React, { useState } from 'react';
+
 //tämä on koodin tehtävänä on luoda BlogContext jonka avulla propseja voidaan välittää mihin vain suoraan.
 
 
 //objekti joka vie tietoa childreneille..
 const BlogContext = React.createContext();
 
-//saa childrenin sisäänsä ja palauttaa siitä objektin
+//saa kaikki childrenit sisäänsä, lisää siihen objektin blogPosts ja muuten palauttaa childrenin normaalisti.
 export const BlogProvider= ({children}) =>{
-    const blogPosts =[
-        {title: 'Blog Post #1'},
-        {title: 'Blog Post #2'},
-    ];
+    const [blogPosts, setBlogPosts]= useState([]);
 
-    return<BlogContext.Provider value={blogPosts}>
+    //käytetään setteriä ja lisätään uusi blogPost.
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, { title : `Blog Post #${blogPosts.length+1}` } ]); //add to arrey syntax
+    }
+    //uusi blogPosts objekti ja callback funktio valuvat alaspäin kaikille childreneille.
+    return(
+        <BlogContext.Provider
+            value={{ data: blogPosts, addBlogPost}}>
         {children}
     </BlogContext.Provider>
+    );
 };
 
 export default BlogContext;
