33ec33f3f6218b3215bcbb38ff7a4efe49f71a47
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-12 13:32:03 +0300

    add Blogpost yhdistetty jsonserveriin.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -56,10 +56,13 @@ const getBlogPosts = dispatch => {
 }
 
 const addBlogPost = dispatch =>{
-    return (title, content, callback) => {
-        dispatch({ type: 'add_blogpost', payload: {title, content} });
-        if(callback){
-            callback()};       //jos callback löytyy niin tee se. Jos ei niin älä tee mitään.
+    return async (title, content, callback) => {
+        await jsonServer.post('/blogposts', {title, content});
+
+
+    //     dispatch({ type: 'add_blogpost', payload: {title, content} });
+    //     if(callback){
+    //         callback()};       //jos callback löytyy niin tee se. Jos ei niin älä tee mitään.
     };
 };
 
